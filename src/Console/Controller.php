a75f74e32abc20671b50c2bb38af6b36f30938eb
viktor <victor_tech@1td.eu>
2021-06-09 09:51:05 +0300

    #hotfix, fix router console

--- src/Console/Controller.php
+++ src/Console/Controller.php
@@ -21,4 +21,50 @@ class Controller extends \Smp\base\Controller
         $this->start = microtime(true);
         echo 'Start: ' . PHP_EOL;
     }
+
+    /**
+     * @param string $message
+     * @param false  $default
+     *
+     * @return bool|mixed
+     */
+    public function confirm(string $message, bool $default = false)
+    {
+        while (true) {
+            static::stdout($message . ' (yes|no) [' . ($default ? 'yes' : 'no') . ']:');
+            $input = trim(static::stdin());
+
+            if (empty($input)) {
+                return $default;
+            }
+
+            if (!strcasecmp($input, 'y') || !strcasecmp($input, 'yes')) {
+                return true;
+            }
+
+            if (!strcasecmp($input, 'n') || !strcasecmp($input, 'no')) {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * @param false $raw
+     *
+     * @return false|string
+     */
+    public static function stdin(bool $raw = false)
+    {
+        return $raw ? fgets(\STDIN) : rtrim(fgets(\STDIN), PHP_EOL);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return false|int
+     */
+    public static function stdout(string $string)
+    {
+        return fwrite(\STDOUT, $string);
+    }
 }
